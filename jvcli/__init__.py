a19173373ffd5e2d22132c4bb7ec81964e1a74c4
Christopher Clarke <christopher.clarke@v75inc.com>
2025-04-20 14:02:35 -0400

    version bump

--- jvcli/__init__.py
+++ jvcli/__init__.py
@@ -4,5 +4,5 @@ jvcli package initialization.
 This package provides the CLI tool for Jivas Package Repository.
 """
 
-__version__ = "2.0.19"
+__version__ = "2.0.20"
 __supported__jivas__versions__ = ["2.0.0"]
