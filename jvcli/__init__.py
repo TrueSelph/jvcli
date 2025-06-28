e19d46581edfcac175581f854575814c1772ef1f
Eldon Marks <eldon.marks@gmail.com>
2025-06-28 09:44:48 -0400

    upgrade branch

--- jvcli/__init__.py
+++ jvcli/__init__.py
@@ -4,5 +4,5 @@ jvcli package initialization.
 This package provides the CLI tool for Jivas Package Repository.
 """
 
-__version__ = "2.0.31"
-__supported__jivas__versions__ = ["2.0.0"]
+__version__ = "2.1.0"
+__supported__jivas__versions__ = ["2.1.0"]
